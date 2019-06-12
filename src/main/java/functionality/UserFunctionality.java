2142d13204c9e98fb3bb733d18b3b5f38187b8b5
flaremew <s185141@student.dtu.dk>
2019-06-12 13:45:46 +0200

    Userfunctionality create user created

--- src/main/java/functionality/UserFunctionality.java
+++ src/main/java/functionality/UserFunctionality.java
@@ -1,16 +1,40 @@
 package functionality;
 
+import dal.UserDAO;
 import dal.interfaces.IUserDAO;
-import dto.*;
+import dto.interfaces.*;
+import services.*;
 
+import java.sql.Connection;
 import java.util.List;
 
 
 public class UserFunctionality implements IUserFunctionality {
     private IUserDAO userDAO;
+    private IDatabaseConnection dbc;
 
-    public UserFunctionality(IUserDAO userDAO){
-        this.userDAO = userDAO;
+    public UserFunctionality(){
+        dbc = new DatabaseConnection();
+        userDAO = new UserDAO();
     }
+
+    public void createUser(IUser user) throws Exception {
+        try {
+            if (user.getUserName().length() < 2 && user.getUserName().length() > 20) {
+                throw new Exception("Username is too short/long.");
+            }
+            if (user.getIni().length() < 2 && user.getIni().length() > 4) {
+                throw new Exception("Initials is too short/long.");
+            }
+            if (user.getCpr().length() != 10 && user.getCpr().charAt(6) != '-') {
+                throw new Exception("CPR is not correctly input.");
+            }
+            Connection conn = dbc.getConnection();
+            userDAO.create(conn, user);
+        } catch (Exception e){
+            throw new Exception("Could not create user: " + e.getMessage());
+        }
+    }
+
 
 }
