bd7b633ac3a6611cd58276fe130cf534960a5049
Ahmad Sattar <thehabbos007@gmail.com>
2019-06-12 15:56:21 +0200

    Raw mat batch Resource

---
+++ src/main/java/web/RawMatBatchResource.java
@@ -0,0 +1,74 @@
+package web;
+
+
+import dto.RawMatBatch;
+import functionality.RawMatBatchFunctionality;
+import functionality.interfaces.IRawMatBatchFunctionality;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/rawMatBatch")
+public class RawMatBatchResource {
+    private IRawMatBatchFunctionality rawMatBatchFunc = new RawMatBatchFunctionality();
+
+    @GET
+    @Path("{productId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response get(@DefaultValue("0") @PathParam("productId") int id) {
+
+        try {
+            return Response.ok(rawMatBatchFunc.getRawMatBatch(id)).build();
+        } catch (Exception e) {
+            return Response.status(Response.Status.NOT_FOUND).entity(e.getMessage()).build();
+        }
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAll() {
+        try {
+            return Response.ok(rawMatBatchFunc.getAllRawMatBatches()).build();
+        } catch (Exception e) {
+            return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
+        }
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response create(RawMatBatch rawmat) {
+        try {
+            rawMatBatchFunc.createRawMatBatch(rawmat);
+            return Response.ok().build();
+        } catch (Exception e) {
+            return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
+        }
+    }
+
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response update(RawMatBatch rawmatBatch) {
+        try {
+            rawMatBatchFunc.updateRawMatBatch(rawmatBatch);
+            return Response.ok().build();
+        } catch (Exception e) {
+            return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
+        }
+    }
+
+    @DELETE
+    @Path("{prodId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response delete(@PathParam("prodId") int id) {
+        try {
+            rawMatBatchFunc.deleteRawMatBatch(rawMatBatchFunc.getRawMatBatch(id));
+            return Response.ok().build();
+        } catch (Exception e) {
+            return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
+        }
+
+    }
+}
