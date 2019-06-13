527bb34d25dae3b4fc5d0491c2761eb0de4a248d
Anders Høiberg Michaelsen <xephyzone@gmail.com>
2019-06-13 14:11:59 +0200

    Create IngredientsResource.java
    Title says all

---
+++ src/main/java/web/IngredientsResource.java
@@ -0,0 +1,81 @@
+package web;
+
+import dto.ProductIngredient;
+import functionality.IngredientsFunctionality;
+import functionality.interfaces.IIngredientsFunctionality;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.awt.*;
+
+/**
+ * Root resource (exposed at "myresource" path)
+ */
+@Path("/ingredients")
+public class IngredientsResource {
+    private IIngredientsFunctionality ingrFunc = new IngredientsFunctionality();
+
+    /**
+     * Method handling HTTP GET requests. The returned object will be sent
+     * to the client as "text/plain" media type.
+     *
+     * @return String that will be returned as a text/plain response.
+     */
+    @GET
+    @Path("{productIngredientId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response get(@DefaultValue("0") @PathParam("productIngredientId") int id) {
+        try {
+            return Response.ok(ingrFunc.getIngredient(id)).build();
+        } catch (Exception e) {
+            return Response.status(Response.Status.NOT_FOUND).entity(e.getMessage()).build();
+        }
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAll() {
+        try {
+            return Response.ok(ingrFunc.getAllIngredient()).build();
+        } catch (Exception e) {
+            return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
+        }
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response create(ProductIngredient ingredient) {
+        try {
+            ingrFunc.createIngredient(ingredient);
+            return Response.ok().build();
+        } catch (Exception e) {
+            return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
+        }
+    }
+
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response update(ProductIngredient ingredient) {
+        try {
+            ingrFunc.updateIngredient(ingredient);
+            return Response.ok().build();
+        } catch (Exception e) {
+            return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
+        }
+    }
+
+    @DELETE
+    @Path("{productIngredientId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response delete(@PathParam("productIngredientId") int id) {
+        try {
+            ingrFunc.deleteIngredient(ingrFunc.getIngredient(id));
+            return Response.ok().build();
+        } catch (Exception e) {
+            return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
+        }
+    }
+}
