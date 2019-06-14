154575559eb9c5adc7ceec269fe586ce7912160d
Ahmad Sattar <thehabbos007@gmail.com>
2019-06-14 13:52:07 +0200

    "es" on batch

--- src/main/java/web/RawMatBatchResource.java
+++ src/main/java/web/RawMatBatchResource.java
@@ -9,7 +9,7 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-@Path("/rawMatBatch")
+@Path("/rawMatBatches")
 public class RawMatBatchResource {
     private IRawMatBatchFunctionality rawMatBatchFunc = new RawMatBatchFunctionality();
 
