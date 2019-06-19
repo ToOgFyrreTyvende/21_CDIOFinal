2ac347f6fcba2df2788cb82b3fc9518d22728e95
Anders Høiberg Michaelsen <xephyzone@gmail.com>
2019-06-19 16:25:58 +0200

    Update IDAO.java
    param names fix xd :ok_hand:

--- src/main/java/dal/interfaces/IDAO.java
+++ src/main/java/dal/interfaces/IDAO.java
@@ -8,22 +8,21 @@ import java.util.List;
 
 public interface IDAO<T> {
 
-    T getObject(Connection conn, int prodBatchId) throws NotFoundException, SQLException;
+    T getObject(Connection conn, int DTOId) throws NotFoundException, SQLException;
 
-    void load(Connection conn, T ProductBatch) throws NotFoundException, SQLException;
+    void load(Connection conn, T DTO) throws NotFoundException, SQLException;
 
     List<T> loadAll(Connection conn) throws SQLException;
 
-    void create(Connection conn, T ProductBatch) throws SQLException;
+    void create(Connection conn, T DTO) throws SQLException;
 
-    void save(Connection conn, T ProductBatch) throws NotFoundException, SQLException;
+    void save(Connection conn, T DTO) throws NotFoundException, SQLException;
 
-    void delete(Connection conn, T ProductBatch) throws NotFoundException, SQLException;
+    void delete(Connection conn, T DTO) throws NotFoundException, SQLException;
 
     void deleteAll(Connection conn) throws SQLException;
 
     int countAll(Connection conn) throws SQLException;
 
-    List<T> searchMatching(Connection conn, T ProductBatch) throws SQLException;
-
+    List<T> searchMatching(Connection conn, T DTO) throws SQLException;
 }
