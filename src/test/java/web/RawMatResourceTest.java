f301390ea1237edd326bb5005943acc84c654c4d
Anders Høiberg Michaelsen <xephyzone@gmail.com>
2019-06-20 12:51:35 +0200

    Create RawMatResourceTest.java
    Created passing tests for `get` & `getAll` methods :ok_hand:

---
+++ src/test/java/web/RawMatResourceTest.java
@@ -0,0 +1,53 @@
+package web;
+
+import dto.RawMat;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+import org.junit.Test;
+
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class RawMatResourceTest extends JerseyTest {
+
+    @Override
+    protected Application configure() {
+        return new ResourceConfig(RawMatResource.class);
+    }
+
+    @Test
+    public void get() {
+        Response response = target("rawMat/42").request().get(Response.class);
+        RawMat content = response.readEntity(RawMat.class);
+
+        assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
+        assertEquals(MediaType.APPLICATION_JSON, response.getHeaderString(HttpHeaders.CONTENT_TYPE));
+        assertEquals(42, content.getRawMatID());
+        assertEquals("dråbelyd+", content.getRawMatName());
+    }
+
+    @Test
+    public void getAll() {
+        Response response = target("rawMat").request().get(Response.class);
+
+        assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
+        assertEquals(MediaType.APPLICATION_JSON, response.getHeaderString(HttpHeaders.CONTENT_TYPE));
+    }
+
+    // @Test
+    public void create() {
+    }
+
+    // @Test
+    public void update() {
+    }
+
+    // @Test
+    public void delete() {
+    }
+}
